22079d86e047bd6423d37ea7d29a00585fb25aca
Ali-Abdulhussein <Ali.Abdulhussein@consid.se>
2025-03-14 16:01:27 +0100

    Use Link and Update Layout page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Blog() {
+  return (
+    <Layout>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-purple-600">Blog Page</h1>
+      </div>
+    </Layout>
+  );
+}
